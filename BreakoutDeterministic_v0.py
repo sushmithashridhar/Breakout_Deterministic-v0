912bef03466d22c4ca4cc16516cf014f42a74cef
Sushmitha Shridhar <shridhar@pdx.edu>
2018-06-07 12:32:18 -0700

    first commit

---
+++ BreakoutDeterministic_v0.py
@@ -0,0 +1,15 @@
+import gym
+
+# Create a breakout environment
+env = gym.make('BreakoutDeterministic-v0')
+# Reset it, returns the starting frame
+frame = env.reset()
+# Render
+env.render()
+
+is_done = False
+while not is_done:
+  # Perform a random action, returns the new frame, reward and whether the game is over
+  frame, reward, is_done, _ = env.step(env.action_space.sample())
+  # Render
+  env.render()
---
+++ README.md
@@ -0,0 +1,1 @@
+# Breakout_Deterministic-v0
